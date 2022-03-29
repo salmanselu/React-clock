9559bbd4229bab81782314a49b8f8f20ff228a4e
= <salmanselu2002@gmail.com>
2022-03-29 15:59:08 +0530

    Added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  let [date, setDate ] = useState(new Date());
+  useEffect(()=>{setDate(new Date())},[date]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="App-logo">
+          {
+            date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+          }
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
